0226ff87f011f1f09b19a01101805c89b10b42e1
Dmitriy Golub <info@mongodb.tech>
2022-06-13 18:14:57 +0000

    Update api.py

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import question, login, users, utils
+from app.api.api_v1.endpoints import login, users, utils
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-api_router.include_router(question.router, prefix="/questions", tags=["questions"])
+
