ee4182de3d0756208916c15d1ed60faf68e75288
Dmitriy Golub <info@mongodb.tech>
2022-06-13 18:30:24 +0000

    Update session.py

--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -4,4 +4,4 @@ import asyncio
 MONGODB_URL = "mongodb://root:mongo@localhost:27017/?authMechanism=DEFAULT"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 client.get_io_loop = asyncio.get_event_loop
-database = client.answeron
+database = client.mongotech
