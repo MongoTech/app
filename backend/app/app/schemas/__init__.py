d6bd6948f1c1c0be58411e0e97fef7885b27893b
Dmitriy Golub <info@mongodb.tech>
2022-06-13 07:12:32 +0000

    Update __init__.py

--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -1,4 +1,3 @@
-from .question import Question, QuestionCreate, QuestionInDB, QuestionUpdate
 from .msg import Msg
 from .token import Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
