9620ff2d828de433c1d17b4e90b5c5c04ab63e8b
Bulat Valiev <b.valiev@platform.live>
2022-06-16 00:06:30 +0200

    add compose deploy to dev

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -1,8 +1,4 @@
 from celery import Celery
 
-MONGO_USER = "root"
-MONGO_PASS = "mongo"
-MONGO_HOST = "localhost"
-MONGO_DB = "answeron"
 options = "authSource=admin&retryWrites=true&w=majority"
-celery = Celery('EOD_TASKS', broker=f'mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:27017/{MONGO_DB}?{options}')
+celery = Celery('EOD_TASKS', broker=f'mongodb://admin:devpass@localhost:27017/fastapi?{options}')
