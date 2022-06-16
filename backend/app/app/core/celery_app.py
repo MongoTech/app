e13c9545aa745dc4c228362bad36802b81189561
Bulat Valiev <b.valiev@platform.live>
2022-06-16 19:47:09 +0200

    fix mongo host

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -1,4 +1,4 @@
 from celery import Celery  # type: ignore
 
 options = "authSource=admin&retryWrites=true&w=majority"
-celery = Celery('EOD_TASKS', broker=f'mongodb://admin:devpass@localhost:27017/fastapi?{options}')
+celery = Celery('EOD_TASKS', broker=f'mongodb://admin:devpass@mongo:27017/fastapi?{options}')
--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -1,7 +1,7 @@
 import motor.motor_asyncio  # type: ignore
 import asyncio
 
-MONGODB_URL = "mongodb://admin:devpass@localhost:27017/?authMechanism=DEFAULT"
+MONGODB_URL = "mongodb://admin:devpass@mongo:27017/?authMechanism=DEFAULT"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 client.get_io_loop = asyncio.get_event_loop
 database = client.mongotech
