99c3a744a2fe9904f342f616f3df8f565be22592
Dmitriy Golub <info@mongodb.tech>
2022-06-25 21:15:41 +0200

    add theme

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-fade-transition mode="out-in">
+    <router-view />
+  </v-fade-transition>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // Styles
+  import '@/styles/overrides.sass'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    metaInfo: {
+      title: 'App',
+      titleTemplate: '%s | Material Dashboard Free',
+      htmlAttrs: { lang: 'en' },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ frontend/src/views/GoogleMaps.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container
+    id="google-maps-view"
+    fluid
+    tag="section"
+  >
+    <v-row>
+      <v-col
+        v-for="(item, i) in maps"
+        :key="i"
+        :md="i !== 0 ? 6 : undefined"
+        cols="12"
+      >
+        <material-card
+          :heading="item.heading"
+          color="accent"
+        >
+          <v-card-text>
+            <v-sheet>
+              <iframe
+                :src="`https://www.google.com/maps/embed?${item.iframe}`"
+                allowfullscreen
+                frameborder="0"
+                height="450"
+                style="border:0"
+                width="100%"
+              />
+            </v-sheet>
+          </v-card-text>
+        </material-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'GoogleMapsView',
+
+    data: () => ({
+      maps: [
+        {
+          heading: 'Satellite Map',
+          iframe: 'pb=!1m18!1m12!1m3!1d26356315.701909266!2d-113.65984645545673!3d36.25591957613731!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited+States!5e0!3m2!1sen!2sus!4v1566158729223!5m2!1sen!2sus',
+        },
+        {
+          heading: 'Satellite Map',
+          iframe: 'pb=!1m18!1m12!1m3!1d193595.15831228695!2d-74.11976206978034!3d40.697663747508045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY!5e0!3m2!1sen!2sus!4v1566158235149!5m2!1sen!2sus',
+        },
+        {
+          heading: 'Custom Skin & Settings Map',
+          iframe: 'pb=!1m18!1m12!1m3!1d193595.15831228695!2d-74.11976206978034!3d40.697663747508045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY!5e0!3m2!1sen!2sus!4v1566158235149!5m2!1sen!2sus',
+        },
+      ],
+    }),
+  }
+</script>
