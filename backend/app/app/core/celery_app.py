4a15ab844f897a80002939117313d87ad8eb51fd
Dmitriy Golub <info@mongodb.tech>
2022-06-30 21:48:58 +0200

    update

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -1,11 +1,11 @@
-from app.core.config import Settings
 from celery import Celery  # type: ignore
 
+from app.core.config import settings
 
-MONGO_URL = f"{Settings.MONGO_USER}:{Settings.MONGO_PASS}@{Settings.MONGO_HOST}:27017"
+MONGO_URL = f"{settings.MONGO_USER}:{settings.MONGO_PASS}@{settings.MONGO_HOST}:27017"
 OPTIONS = "authSource=admin&retryWrites=true&w=majority"
 
 celery = Celery(
     "EOD_TASKS",
-    broker=f"mongodb://{MONGO_URL}/{Settings.MONGO_DB}?{OPTIONS}",
+    broker=f"mongodb://{MONGO_URL}/{settings.MONGO_DB}?{OPTIONS}",
 )
--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -1,8 +1,12 @@
-from app.core.config import Settings
 import asyncio
+
 import motor.motor_asyncio  # type: ignore
 
-MONGO_URL = f"{Settings.MONGO_USER}:{Settings.MONGO_PASS}@{Settings.MONGO_HOST}:27017"
-client = motor.motor_asyncio.AsyncIOMotorClient(f"mongodb://{MONGO_URL}/?authMechanism=DEFAULT")
+from app.core.config import settings
+
+MONGO_URL = f"{settings.MONGO_USER}:{settings.MONGO_PASS}@{settings.MONGO_HOST}:27017"
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    f"mongodb://{MONGO_URL}/?authMechanism=DEFAULT"
+)
 client.get_io_loop = asyncio.get_event_loop
 database = client.mongotech
