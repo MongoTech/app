2625d8964755ed9d1a3251ceaadfd3265c1d2291
Dmitriy Golub <dmitriy.golub@gmail.com>
2023-03-21 22:38:29 +0100

    Delete get-updated-badge.sh

--- get-updated-badge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "collecting stas for badges"
-
-commits=`git rev-list --all --count`
-
-#latest_release_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-#echo "{\"commits\":\"$commits\", \"release_tag"\:\"$latest_release_tag\"}" > badges.json
-echo "{\"message\":\"$1\"}" > "./$1-badges.json"
